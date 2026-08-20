290f2227a08e8fa9d2ecb417b34b35e8751d2c90
Gabriel John Solomon <153481400+Gabi-comm@users.noreply.github.com>
2026-08-20 22:00:56 +0800

    update checks

--- backend/mod_check.py
+++ backend/mod_check.py
@@ -1,6 +1,12 @@
 
 from ultralytics import YOLO
 
+from roboflow import Roboflow
+rf = Roboflow(api_key="JVjvj0TbGk9Sr2npNrr0")
+project = rf.workspace("gabriel-zisua").project("road-atwrz")
+version = project.version(3)
+dataset = version.download("yolov8")
+                
 model=YOLO(r"C:\Users\Gab\Documents\GitHub\NeuroSymbolic\backend\ultrabestroad.pt")
 results=model.val(data=r"C:\Users\Gab\Documents\GitHub\NeuroSymbolic\backend\data.yaml")
 print(f"Precision (All classes): {results.results_dict['metrics/precision(B)']:.4f}")
